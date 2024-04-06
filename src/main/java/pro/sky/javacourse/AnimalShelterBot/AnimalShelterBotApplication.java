8ac17f3674bbed181dc433b300bec7467c2b200f
Pavel-Fomchenkov <donotwritemail@gmail.com>
2024-04-06 23:07:38 +0300

    Initial commit

---
+++ src/main/java/pro/sky/javacourse/AnimalShelterBot/AnimalShelterBotApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.javacourse.AnimalShelterBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterBotApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		SpringApplication.run(AnimalShelterBotApplication.class, args);
+	}
+
+}
